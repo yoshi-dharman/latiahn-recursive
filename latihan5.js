df5160901be36b3a72264971bd5ea1f9a7ac3658
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-02-03 18:42:08 +0700

    fix latihan 5 yang salah

--- latihan5.js
+++ latihan5.js
@@ -1,14 +1,16 @@
-let arrayDuplikasi = [];
-
 function angkaDuplikasi(n,item){
+    let arrayDuplikasi = [];
+
     if(n <= 0){
-        return "";
+        return [];
     }else{
+        arrayDuplikasi = angkaDuplikasi(n-1,item);
         arrayDuplikasi.push(item);
-        angkaDuplikasi(n-1,item);
         return arrayDuplikasi;
     }
 }
 
 console.log(angkaDuplikasi(0,3));
+console.log(angkaDuplikasi(3,7));
 console.log(angkaDuplikasi(5,7));
+console.log(angkaDuplikasi(-1,7));
